ace1c1a2019c9dc5ce87aa2d17dec059960c13e1
ArinaMr <arinamandrik@gmail.com>
2025-11-05 11:24:57 +0300

    Fix

--- src/main/java/org/ispirer/xmldatabase/model/StatAttribute.java
+++ src/main/java/org/ispirer/xmldatabase/model/StatAttribute.java
@@ -2,11 +2,13 @@ package org.ispirer.xmldatabase.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "stat_attribute")
 public class StatAttribute {
     @Id
--- src/main/java/org/ispirer/xmldatabase/model/StatElement.java
+++ src/main/java/org/ispirer/xmldatabase/model/StatElement.java
@@ -2,6 +2,7 @@ package org.ispirer.xmldatabase.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "stat_element")
 public class StatElement {
     @Id
